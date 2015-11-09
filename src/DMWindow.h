ea4623c3a2aae5bcd4ec1d1e0afcd30f41e3a749
Yinka Ashon <gigalimit20@yahoo.com>
2015-11-09 22:42:24 +1100

    FS005 Added initial base window files

---
+++ src/DMWindow.h
@@ -0,0 +1,32 @@
+/**
+ * File:   DMWindow.h
+ * @author Yinka Ashon
+ * @created 2015-11-08
+ */
+#ifndef __FSWINDOW_H__
+#define	__FSWINDOW_H__
+
+#include <gtkmm/window.h>
+
+namespace DupManager {
+
+    class DMWindow {
+    public:
+        DMWindow();
+        DMWindow(const DMWindow& orig);
+        virtual ~DMWindow();
+        
+        int run(int argc, char * argc[]);
+    private:
+        
+    protected:
+        void setupWindow(const char*);
+        
+        
+        Glib::RefPtr<Gtk::Application> m_application = nullptr_t;
+        Gtk::Window m_window = nullptr_t;
+
+    };
+};
+#endif	/* __FSWINDOW_H__ */
+
